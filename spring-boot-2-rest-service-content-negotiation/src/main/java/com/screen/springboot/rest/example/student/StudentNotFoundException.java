098db413626461d59431669df04b29a7b200a85e
vvaradar <vijayaganesh.varadarajamuthukumar@ironmountain.com>
2021-03-22 10:15:58 -0400

    pushing files

---
+++ spring-boot-2-rest-service-content-negotiation/src/main/java/com/screen/springboot/rest/example/student/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+package com.screen.springboot.rest.example.student;
+
+public class StudentNotFoundException extends RuntimeException {
+
+	public StudentNotFoundException(String exception) {
+		super(exception);
+	}
+
+}
---
+++ spring-boot-2-rest-service-content-negotiation/src/main/java/com/screen/springboot/rest/example/student/StudentResource.java
@@ -0,0 +1,55 @@
+package com.screen.springboot.rest.example.student;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+public class StudentResource {
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@GetMapping("/students")
+	public List<Student> retrieveAllStudents() {
+		return studentRepository.findAll();
+	}
+
+	/* Complete this to retrieve  a  student  based on id */
+	public Student retrieveStudent(long id) {
+        return null;
+	}
+
+	@DeleteMapping("/students/{id}")
+	public void deleteStudent(@PathVariable long id) {
+		studentRepository.deleteById(id);
+	}
+
+	@PostMapping("/students")
+	public ResponseEntity<Object> createStudent(@RequestBody Student student) {
+		Student savedStudent = studentRepository.save(student);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(savedStudent.getId()).toUri();
+
+		return ResponseEntity.created(location).build();
+
+	}
+	
+    /* Make this complete to create a new student */
+	public ResponseEntity<Object> updateStudent(@RequestBody Student student, @PathVariable long id) {
+
+        return null;
+	}
+}
